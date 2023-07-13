2188b7782709d9a816d515dd8a3835f7b3ec4b91
Harpreet Singh <harpreetsinghdev13@gmail.com>
2023-07-14 01:05:28 +1200

    alternate home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
 
   return (
     <main className="">
-      <ImageSlider />
       <About />
       <PastEvents />
       <Contact />
@@ -23,29 +22,33 @@ const ImageSlider = () => {
   ];
 
   return (
-    <SimpleImageSlider
-      width={'972px'}
-      height={'648px'}
-      images={images}
-      showBullets={false}
-      showNavs={true}
-      autoPlay
-      autoPlayDelay={5}
-    />
+    <div className="rounded">
+      <SimpleImageSlider
+        width={'729px'}
+        height={'486px'}
+        images={images}
+        showBullets={false}
+        showNavs={true}
+        autoPlay
+        autoPlayDelay={5}
+      />
+    </div>
   )
 }
 
 const About = () => {
   return (
-    <div className="bg-sky-100 text-center h-[60vh] flex flex-col items-center justify-center">
-      <div className="font-medium text-2xl pb-8">About</div>
-      <div className="max-w-2xl text-xl font-light">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <div className="bg-sky-100 flex justify-between items-center">
+      <div className="flex flex-col pl-10">
+        <div className="max-w-2xl text-xl font-light">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
       </div>
+      <ImageSlider />
     </div>
   )
 }
