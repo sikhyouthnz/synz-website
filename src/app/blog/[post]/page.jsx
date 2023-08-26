6aa504f5dc9d02ef162fc043a8cf7f21fee0cb93
Harpreet Singh <harpreetsinghdev13@gmail.com>
2023-08-26 14:35:55 +1200

    minor style update

--- src/app/blog/[post]/page.jsx
+++ src/app/blog/[post]/page.jsx
@@ -3,12 +3,13 @@ export default function Page({ params }) {
     const Content = markdownContext.react;
     console.log(markdownContext)
     return (
-        <div className="flex flex-col justify-center items-center p-7 md:px-60 text-lg">
-            <div className="font-medium text-3xl">{markdownContext.attributes.title}</div>
-            <br/>
-            <div className="font-medium text-lg text-gray-500">{markdownContext.attributes.date}</div>
-            <div className="font-normal text-lg text-gray-600">{markdownContext.attributes.summary}</div>
-            <br/>
+        <div className="p-7 md:px-60 text-lg">
+            <div className="flex flex-col justify-center items-center gap-3">
+                <div className="font-medium text-3xl">{markdownContext.attributes.title}</div>
+                <div className="font-medium text-lg text-gray-500">{markdownContext.attributes.date}</div>
+                <div className="font-normal text-lg text-gray-600 text-center">{markdownContext.attributes.summary}</div>
+            </div>
+            <hr class="w-48 h-1 mx-auto my-4 bg-gray-300 border-0 rounded md:my-10"></hr>
             <Content />
         </div>
     )
