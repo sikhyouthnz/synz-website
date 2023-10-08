ef11aa025d6296cfbdd7c7dc905ca27dc1b383be
Harpreet Singh <harpreetsinghdev13@gmail.com>
2023-10-08 20:34:10 +1300

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata = {
   title: 'SYNZ - Sikh Youth New Zealand',
-  description: 'SYNZ: Empowering Sikh Youth in New Zealand',
+  description: "SYNZ is a registered charity empowering Sikh youths in New Zealand. Our website showcases annual events, career guidance, mental health talks, and more. We're a trusted, multicultural organization embodying Guru's unity message. Explore how SYNZ supports Sikh youth today.",
 }
 
 export default function RootLayout({
