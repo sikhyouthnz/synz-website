2c32a66df61cce40425dd43c7559c0738ba9248e
Harpreet Singh <harpreetsinghdev13@gmail.com>
2024-06-29 17:11:30 +1200

    add colours

--- src/app/blog/[post]/page.jsx
+++ src/app/blog/[post]/page.jsx
@@ -2,6 +2,9 @@ export default function Page({ params }) {
     const markdownContext = require('../../../../content/blog/' + params.post + '.md');
     const Content = markdownContext.react;
 
+    const Orange = ({children}) => <span style={{color: "#F28C28"}}>{children}</span>
+    const Green = ({children}) => <span style={{color: "#097969"}}>{children}</span>
+
     return (
         <div className="p-7 md:px-60 text-lg flex flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-3">
@@ -9,7 +12,7 @@ export default function Page({ params }) {
             </div>
             <hr className="w-48 h-1 mx-auto my-4 bg-gray-300 border-0 rounded md:my-10"></hr>
             <div className="prose lg:prose-xl">
-                <Content />
+                <Content Orange={Orange} Green={Green} />
             </div>
         </div>
     )
