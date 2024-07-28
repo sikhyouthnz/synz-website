79fdb2784310603c00aaa89ef39e4ca75e80f5c0
Harpreet Singh <harpreetsinghdev13@gmail.com>
2024-07-28 21:12:42 +1200

    add author name

--- src/app/blog/[post]/page.jsx
+++ src/app/blog/[post]/page.jsx
@@ -9,6 +9,7 @@ export default function Page({ params }) {
         <div className="p-7 md:px-60 text-lg flex flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-3">
                 <div className="font-medium text-3xl">{markdownContext.attributes.title}</div>
+                <div className="font-normal text-slate-900 text-xl">- Hao-Wei Shu</div> 
             </div>
             <hr className="w-48 h-1 mx-auto my-4 bg-gray-300 border-0 rounded md:my-10"></hr>
             <div className="prose lg:prose-xl">
